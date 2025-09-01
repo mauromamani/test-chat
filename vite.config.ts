07366d4e0915e196c9cfec09a32ad782d0910c91
mauromamani <mauromamani.b@gmail.com>
2025-08-31 21:44:46 -0300

    123

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "AIChatbot",
-      fileName: (format) => `ai-chatbot.${format}.js`,
+      fileName: () => "index.js"
     },
     rollupOptions: {
       external: ["react", "react-dom"],
